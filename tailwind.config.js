809c2c1bc2b7812b7ff54352f4833e5b86174d42
prince475 <danielmwai475@gmail.com>
2024-10-02 13:34:15 +0300

    Adding tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+// tailwind.config.js
+export const content = [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}',
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
