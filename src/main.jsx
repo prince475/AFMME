cce4ecd4a7b2f732b5a64f1c0c2890875eaa4b04
prince475 <danielmwai475@gmail.com>
2024-10-02 13:36:50 +0300

    Performing routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+// import Contact from "./pages/contact/Contact.jsx";
+// import Programs from "./pages/programs/Programs.jsx";
+// import SupportUs from "./pages/support-us/SupportUs.jsx";
+// import Staff from "./pages/staff/Staff.jsx";
+import Home from "./components/Home.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "home",  
+        element: <Home />,
+      },
+    //   {
+    //     path: "programs",
+    //     element: <Programs />,
+    //   },
+    //   {
+    //     path: "support-us",
+    //     element: <SupportUs />,
+    //   },
+    //   {
+    //     path: "staff",
+    //     element: <Staff />,
+    //   },
+    //   {
+    //     path: "contact",
+    //     element: <Contact />,
+    //   },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
+
